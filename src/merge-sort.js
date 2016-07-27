ad45fbbb669680db006ea370896822f416ae0828
Steven Bassett <steven.j.bassett@gmail.com>
2016-07-26 18:25:05 -0700

    Update: add merge sort test

--- src/merge-sort.js
+++ src/merge-sort.js
@@ -4,25 +4,24 @@ export function merge(left: Array<number>, right: Array<number>): Array<number> 
     let result: Array<number> = [];
     let iRight: number = 0;
     let iLeft: number = 0;
-    while ((iRight < right.length) && (iLeft < left.length)) {
-        if (left[iRight] < right[iLeft]) {
+    while (iLeft < left.length && iRight < right.length) {
+        if (left[iLeft] < right[iRight]) {
             result.push(left[iLeft++]);
         } else {
             result.push(right[iRight++])
         }
     }
 
-    return result.concat(left.slice(iLeft)).concat(right.slice(iRight))
+    return result.concat(left.slice(iLeft)).concat(right.slice(iRight));
 }
-
 
 export default function mergeSort(arr: Array<number>): Array<number> {
     const { length } = arr;
     if (length < 2) return arr;
 
     const middle: number = Math.floor(length / 2);
-    const left: Array<number> = arr.splice(0, middle);
-    const right: Array<number> = arr.splice(middle);
-    console.log(middle, left, right);
+    const left: Array<number> = arr.slice(0, middle);
+    const right: Array<number> = arr.slice(middle);
+
     return merge(mergeSort(left), mergeSort(right));
 }
--- src/merge-sort_test.js
+++ src/merge-sort_test.js
@@ -7,8 +7,13 @@ test('Merge Function', (t) => {
     const result = merge([1, 2, 3], [1, 2, 3]);
     t.deepEqual(result, [1, 1, 2, 2, 3, 3]);
 });
-test('Merge Sort', (t) => {
-    t.plan(1);
-    t.deepEqual(mergeSort([100, 90, 80, 10]), [10, 80, 90, 100]);
 
+test('Merge Sort', (t) => {
+    t.plan(2);
+    t.deepEqual(mergeSort([100, 90, 80, 10]), [10, 80, 90, 100]);
+    const huge = [];
+    for (let i = 0; i < 1E5; i++) {
+        huge[i] = Math.floor(Math.random() * (1E7 - 1)) + 1;
+    }
+    t.deepEqual(mergeSort(huge), huge.sort((a, b) => a - b));
 });
